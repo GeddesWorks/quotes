0571f85aed599ca0ff47430fdae66c194222e464
GeddesWorks <collin.geddes@gmail.com>
2025-02-22 22:43:40 -0600

    Edit vite config

--- QuotesManager/vite.config.ts
+++ QuotesManager/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // Replace 'your-repo-name' with the actual repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/quotes/',  // ✅ Important for GitHub Pages
+  base: "/",
 });
